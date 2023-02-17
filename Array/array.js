cae476420fc79e3f4947dc8b62b9425dd589f3e6
AlanFerPcNet <fernandeezalan20@gmail.com>
2023-02-17 10:03:52 -0300

    HashTable

--- Array/array.js
+++ Array/array.js
@@ -14,7 +14,24 @@ class MyArray {
     }
 
     pop(){
-        
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length -1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete (index){
+        const item = this.data[index];
+        this.shiftIndex(index);
+        return item;
+    }
+
+    shiftIndex(index){
+        for(let i = index; i < this,this.length-1; i++){
+            this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length -1];
+        this.length --;
     }
 }
 
@@ -22,6 +39,7 @@ const myArray = new MyArray();
 
 myArray.push('pedro');
 myArray.push('juan');
+myArray.push('Robert');
 
-console.log(myArray.get(1));
+console.log(myArray.get(2));
 
