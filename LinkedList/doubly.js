c488daca890d29c4d4a4e94f24b77188de4ebca8
Alan D Fernandez <fernandeezalan20@gmail.com>
2023-02-22 10:13:45 -0300

    LinkedList doubly

--- LinkedList/doubly.js
+++ LinkedList/doubly.js
@@ -1,15 +1,4 @@
-// let singlyLinkedList = {
-//     head: {
-//         value: 1,
-//         next: {
-//             value: 2,
-//             next:{
-//                 value: 3,
-//                 next: null,
-//             }
-//         }
-//     }
-// }
+// Una doubly linked list tiene el mismo comportamiento que una simply linked list, pero con la particularidad de que esta SI puede regresar. Aún necesita ir moviéndose una por una, pero ahora si lo desea puede regresar porque ya conoce quién es su elemento anterior y también quién es el siguiente, es decir, ya no es necesario repetir el ciclo.
 
 class Nodo {
     constructor(value){
